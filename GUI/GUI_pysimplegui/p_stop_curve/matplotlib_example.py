10c21088ff838fb80e699a3edb5efa7714e114b7
kassieclaire <59930829+kassieclaire@users.noreply.github.com>
2021-11-19 09:23:35 -0600

    GUI updated to look more like the drawings

--- GUI/GUI_pysimplegui/p_stop_curve/matplotlib_example.py
+++ GUI/GUI_pysimplegui/p_stop_curve/matplotlib_example.py
@@ -12,7 +12,12 @@ from p_stop_curve import cascading_failure_function
     launches a MatplotLib window that is interactive with the usual Matplotlib controls.
     It turns out to be a rather simple thing to do.  The secret is to add parameter block=False to plt.show()
 """
-
+#define values here
+cap_loss = 1500
+delivery_loss_percent = 8
+worst_cluster = 4
+num_lines = 186
+#end defines
 def draw_plot():
     p_stop_df = cascading_failure_function()
     fig = plt.figure()
@@ -44,14 +49,24 @@ def draw_figure(canvas, figure):
 #           [sg.Button('Cancel')]]
 #create column
 #fill in column
-input_column = [[sg.Text('Load: '), sg.InputText()],
-                [sg.Text('Line Failures: '), sg.InputText()],
-                [sg.Text('Load Shed Control: '), sg.InputText()],
-                [sg.Text('Cap Est. Err.: '), sg.InputText()],
-                [sg.Button('Run'), sg.Button('More Options')],
-                [sg.Button('Cancel')]]
-layout = [[sg.Text('Plot test')],
-          [sg.Column(input_column, element_justification='c'), sg.Canvas(key='-CANVAS-')]]
+input_column = [[sg.Text('Load')],
+                [sg.Slider(orientation ='horizontal', key='stSlider', range=(0.0,1.0), resolution=.01)],
+                [sg.Text('Initial Line Failures: ')],
+                [sg.Slider(orientation ='horizontal', key='stSlider', range=(1,num_lines), resolution=1)],
+                [sg.Text('Load Shed Control')],
+                [sg.Slider(orientation ='horizontal', key='stSlider', range=(0.0,1.0), resolution=.01)],
+                [sg.Text('Cap Est. Err.')],
+                [sg.Slider(orientation ='horizontal', key='stSlider', range=(0.0,1.0), resolution=.01)],
+                [sg.Button('More Options'), sg.Button('Run')],
+                ]
+output_column = [[sg.Canvas(key='-CANVAS-')],
+                [sg.Text('Loss of Delivery Capacity: '), sg.Text(str(delivery_loss_percent) + "%")],
+                [sg.Text('Max Line Capacity: '), sg.Text(str(cap_loss) + " MW")],
+                [sg.Text('Worst-off Cluster: '), sg.Text(str(worst_cluster))],
+                [sg.Text('Probability of failure: '), sg.Text('Click on Line')],
+                ]
+layout = [[sg.Text('Cascading failure sim')],
+          [sg.Column(input_column, element_justification='c'), sg.Column(output_column, element_justification='c')]]
 # create the form and show it without the plot
 window = sg.Window('Demo Application - Embedding Matplotlib In PySimpleGUI', layout, finalize=True, element_justification='center', font='Helvetica 18')
 
