e737bdbc455a19f6bbd1201703411a9620de5879
Brendan Nenninger <brendannenn@gmail.com>
2022-03-27 21:29:14 -0500

    Adds a network topology graphing utility
    Utility takes in the index of a simulation and the index of an iteration of that simulation and generates a color-coded network of the failures at that point in the simulation.
    
    The demo has been fixed so it does not restart the window.

--- Development/generate_mpc_plot_networkx.py
+++ Development/generate_mpc_plot_networkx.py
@@ -1,15 +1,21 @@
 
 
+import enum
+from json import load
 from random import seed
+from urllib import request
+from xmlrpc.client import Boolean
 import matplotlib.pyplot as plt
 import networkx as nx
 import scipy.io
 import pandas as pd
 
+import load_sim_data
+
 
 MPC_PATH = 'case118_mpc_presim.mat'
-SIM_STATE_MATRIX = 'case118_f2_r2_t1_e1_i100000_sm.mat'
-SIM_INITIAL_FAILURES = 'case118_f2_r2_t1_e1_i100000_if.mat'
+SIM_STATE_MATRIX = 'case118_f2_r7_t1_e1_i100000_sm.mat'
+SIM_INITIAL_FAILURES = 'case118_f2_r7_t1_e1_i100000_if.mat'
 
 SEED = 42
 
@@ -32,31 +38,191 @@ def get_branch_dataframe(mpc_matrix):
     return branch_data
 
 
+def get_statematrix_steady_state_negative_one_indices(state_matrix) -> 'list[int]':
+    return [i for i, x in enumerate(state_matrix['Steady State']) if x == -1]
+
+
+def get_sim_index_with_most_failures(line_failed_negative_one_indices) -> int:
+    maxFailures = 0
+    maxIndex = 0
+    for i in range(1, len(line_failed_negative_one_indices)):
+        numFailures = line_failed_negative_one_indices[i] - \
+            line_failed_negative_one_indices[i - 1] - 1
+        if(numFailures > maxFailures):
+            maxFailures = numFailures
+            maxIndex = i
+    return maxIndex
+
+
+def get_simulation_start_end_iterations(line_failed_negative_one_indices, simulation_index):
+    startIndex = line_failed_negative_one_indices[simulation_index -
+                                                  1] + 1 if simulation_index > 0 else 0
+    endIndex = line_failed_negative_one_indices[simulation_index] + 1
+    iterations = endIndex - startIndex
+    return startIndex, endIndex, iterations
+
+
+def get_single_simulation_state_matrix(state_matrix, line_failed_negative_one_indices, simulation_index):
+    startIndex, endIndex, _ = get_simulation_start_end_iterations(
+        line_failed_negative_one_indices, simulation_index)
+    # startIndex = line_failed_negative_one_indices[simulation_index -
+    #                                               1] + 1 if simulation_index > 0 else 0
+    # endIndex = line_failed_negative_one_indices[simulation_index] + 1
+    return state_matrix[startIndex:endIndex]
+
+# def get_failure_labels_at_index(self, iteration: int, failedLabel, liveLabel) -> 'list[int]':
+#         failures = self.get_failures_until_iteration(iteration)
+#         return [failedLabel if i in failures else liveLabel for i in range(self.total_lines)]
+
+
+def get_failure_array_at_iteration(initial_failures, state_matrix, line_failed_negative_one_indices, simulation_index, iteration_index):
+    state_matrix = get_single_simulation_state_matrix(
+        state_matrix, line_failed_negative_one_indices, simulation_index)
+    # initial_failures = initial_failures[simulation_index]
+    failures = initial_failures[simulation_index] + \
+        state_matrix['Failed Line Index'][1:iteration_index + 1].tolist()
+    return failures
+
+
+def labels_at_indices(arr: 'list[int]', val_if_present, val_if_not_present, num_indices) -> list:
+    return [val_if_present if i in arr else val_if_not_present for i in range(num_indices)]
+
+
+def plot_network(mpc_branch_dataframe, initial_failures, state_matrix, line_failed_negative_one_indices, simulation_index, iteration_index, bus_labels: bool = True, branch_labels: bool = False, axes=None, fig=None):
+    if fig is None:
+        fig = plt.figure()
+    else:
+        fig = plt.figure(fig.number)
+    failure_indix_arr = get_failure_array_at_iteration(
+        initial_failures, state_matrix, line_failed_negative_one_indices, simulation_index, iteration_index)
+    # shift the indices down 1 as python is zero-based and therefore networkx will be looking for zero-based indices
+    failure_indix_arr = [x - 1 for x in failure_indix_arr]
+    colors = labels_at_indices(
+        failure_indix_arr, 'r', 'b', mpc_branch_dataframe.shape[0])
+    edges = [(branch_data['from bus number'][i], branch_data['to bus number'][i])
+             for i in range(branch_data.shape[0])]
+    g = nx.Graph()
+    g.add_edges_from(edges)
+    edge_labels = {edges[i]: i + 1 for i in range(branch_data.shape[0])}
+    # TODO change the weight to reflect load
+    weights = [5 if i == 'r' else 1 for i in colors]
+    pos = nx.kamada_kawai_layout(g)
+    nx.draw(g, pos=pos, ax=axes, with_labels=bus_labels, node_size=60,
+            font_size=8, edge_color=colors, width=weights)
+    if branch_labels:
+        nx.draw_networkx_edge_labels(
+            g, ax=axes, pos=pos, edge_labels=edge_labels, font_size=6, rotate=False)
+    # plt.show()
+    return fig
+
+
 mpc_matrix = load_mpc(MPC_PATH)
 branch_data = get_branch_dataframe(mpc_matrix)
 
-print(branch_data)
+# print(branch_data)
 
-edges = [(branch_data['from bus number'][i], branch_data['to bus number'][i])
-         for i in range(branch_data.shape[0])]
+# edges = [(branch_data['from bus number'][i], branch_data['to bus number'][i])
+#          for i in range(branch_data.shape[0])]
 
-print(edges)
+# # print(edges)
 
-g = nx.Graph()
+# g = nx.Graph()
 
-g.add_edges_from(edges)
+# g.add_edges_from(edges)
 
-colors = ['r' if i % 10 == 0 else 'b' for i in range(branch_data.shape[0])]
-weights = [5 if i == 'r' else 1 for i in colors]
-print(colors)
+# edge_labels = {edges[i]: i + 1 for i in range(branch_data.shape[0])}
+# # print(edge_labels)
 
-# pos = nx.spring_layout(g, seed=SEED)
-pos = nx.kamada_kawai_layout(g)
+# colors = ['r' if i % 10 == 0 else 'b' for i in range(branch_data.shape[0])]
+# weights = [5 if i == 'r' else 1 for i in colors]
+# # print(colors)
+
+# # pos = nx.spring_layout(g, seed=SEED)
+# pos = nx.kamada_kawai_layout(g)
 
 
-nx.draw(g, pos=pos, with_labels=True, node_size=60,
-        font_size=8, edge_color=colors, width=weights)
-plt.show()
+# nx.draw(g, pos=pos, with_labels=True, node_size=60,
+#         font_size=8, edge_color=colors, width=weights)
+# nx.draw_networkx_edge_labels(
+# g, pos, edge_labels=edge_labels, font_size=6, rotate=False)
+# plt.show()
 
-print(max(branch_data['from bus number']))
-print(max(branch_data['to bus number']))
+# print(max(branch_data['from bus number']))
+# print(max(branch_data['to bus number']))
+
+state_matrix = load_sim_data.load_state_matrix(SIM_STATE_MATRIX)
+initial_failures = load_sim_data.load_initial_failures(SIM_INITIAL_FAILURES)
+
+# state_matrix = state_matrix[:100]
+# initial_failures = initial_failures[:100]
+
+# print(state_matrix['Failed Line Index'])
+# print(max(state_matrix['Failed Line Index']))
+# print(initial_failures)
+
+# generate_simulation_history_array(state_matrix, initial_failures)
+print(state_matrix.shape)
+# print([state_matrix['Failed Line Index'][i]
+#   for i in range(state_matrix.shape[0])])
+
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    print(state_matrix[['Steady State', 'Failed Line Index']])
+
+negativeOneIndices = get_statematrix_steady_state_negative_one_indices(
+    state_matrix)
+
+print([x for x in zip(negativeOneIndices, range(len(negativeOneIndices)))])
+# print(get_single_simulation_state_matrix(state_matrix, zeroIndices, 30))
+print(get_sim_index_with_most_failures(negativeOneIndices))
+# print(get_single_simulation_state_matrix(state_matrix, zeroIndices, get_sim_index_with_most_failures(zeroIndices)))
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    print(get_single_simulation_state_matrix(state_matrix, negativeOneIndices,
+          get_sim_index_with_most_failures(negativeOneIndices)))
+    # print(get_single_simulation_state_matrix(state_matrix, negativeOneIndices, 0))
+
+for i in range(10):
+    print(get_failure_array_at_iteration(initial_failures, state_matrix,
+          negativeOneIndices, get_sim_index_with_most_failures(negativeOneIndices), i))
+
+print()
+
+print(labels_at_indices([1, 2, 6], 'present', 'not present', 15))
+
+# fig = plot_network(branch_data, initial_failures, state_matrix,
+#                    negativeOneIndices, get_sim_index_with_most_failures(negativeOneIndices), 4, True, False)
+# fig.show()
+
+# plt.figure(fig.number)
+# plt.show()
+
+most_failure_sim_index = get_sim_index_with_most_failures(negativeOneIndices)
+iteration = 0
+_, _, numIterations = get_simulation_start_end_iterations(
+    negativeOneIndices, most_failure_sim_index)
+print(f"{numIterations} iterations")
+plt.ion()
+fig = plt.figure()
+# ax = plt.axes()
+ax = None
+print(fig.axes)
+plt.show(block=False)
+while(1):
+    # for i in range(numIterations):
+    #     fig = plot_network(branch_data, initial_failures, state_matrix,
+    #                        negativeOneIndices, get_sim_index_with_most_failures(negativeOneIndices), iteration, True, False)
+    #     plt.figure(fig.number)
+    #     plt.show(block=False)
+
+    action = input("w to advance, s to go back, x to exit")
+    if action == 'w':
+        iteration += 1
+        # plt.close()
+    elif action == 's':
+        iteration -= 1
+        # plt.close()
+    elif action == 'x':
+        break
+    fig.clear()
+    fig = plot_network(branch_data, initial_failures, state_matrix,
+                       negativeOneIndices, get_sim_index_with_most_failures(negativeOneIndices), iteration, True, False, ax, fig)
+    # plt.figure(fig.number)
---
+++ Development/load_sim_data.py
@@ -0,0 +1,105 @@
+
+import pandas as pd
+import numpy as np
+import scipy.io
+
+from sim_failure_structure import Simulation_Line_History
+
+SIM_STATE_MATRIX = 'case118_f2_r7_t1_e1_i100000_sm.mat'
+SIM_INITIAL_FAILURES = 'case118_f2_r7_t1_e1_i100000_if.mat'
+
+
+def load_state_matrix(states_matrix_name='states', number_of_lines=186):
+    # Temporarily loading in matrix
+    # the states matrix input -- temporary
+    mat = scipy.io.loadmat(states_matrix_name)
+    states_column_names = ['Total Line Failures', 'Maximum failed line capacity',
+                           'Load shed from previous step', 'Difference in Load Shed',
+                           'Load', 'Free Space 1', 'Free Space 2', 'Steady State',
+                           'Capacity of Failed Ones', 'Capacity of Failed Ones 2', 'Failed Line Index',
+                           'Capacity of Failed One', 'Time of Failure Event',
+                           'Accumulation of Failed Capacities', 'Free Space 3', 'Demand-Loadshed Difference',
+                           'Free Space 4', 'Generation']
+    data = mat['States']
+    states_df = pd.DataFrame(data=data, columns=states_column_names)
+    states_df = states_df.astype({'Total Line Failures': int, 'Maximum failed line capacity': float,
+                                  'Load shed from previous step': float, 'Difference in Load Shed': float,
+                                  'Load': float, 'Free Space 1': int, 'Free Space 2': int, 'Steady State': int,
+                                  'Capacity of Failed Ones': float, 'Capacity of Failed Ones 2': float, 'Failed Line Index': int,
+                                  'Capacity of Failed One': float, 'Time of Failure Event': float,
+                                  'Accumulation of Failed Capacities': float, 'Free Space 3': int, 'Demand-Loadshed Difference': float,
+                                  'Free Space 4': int, 'Generation': float})
+    states_df.drop(columns=['Free Space 1', 'Free Space 2',
+                   'Free Space 3', 'Free Space 4'], inplace=True)
+
+    return states_df
+
+    # # Save the dataframe
+    # states_df.to_csv(r'states_dataframe.csv', index=False)
+    # states_simple_df = states_df.copy()
+    # states_simple_df.to_csv(r'states_simple.csv', index=False)
+    # states_simple_df.drop(columns=['Maximum failed line capacity',
+    #                                'Load shed from previous step', 'Difference in Load Shed',
+    #                                'Load', 'Capacity of Failed Ones', 'Capacity of Failed Ones 2', 'Failed Line Index',
+    #                                'Capacity of Failed One', 'Time of Failure Event', 'Demand-Loadshed Difference',
+    #                                'Generation'], inplace=True)
+    # total_states = [0] * (number_of_lines + 1)
+    # stable_states = [0] * (number_of_lines + 1)
+    # for index, row in states_df.iterrows():
+    #     #print(row['Total Line Failures'])
+    #     total_line_failures = int(row['Total Line Failures'])
+    #     steady_state = int(row['Steady State'])
+    #     if(total_line_failures > 0):
+    #         total_states[total_line_failures] = total_states[total_line_failures] + 1
+    #         if (steady_state == -1):
+    #             stable_states[total_line_failures] = stable_states[total_line_failures] + 1
+    # cascade_stop = [0]*(number_of_lines + 1)
+    # for i in range(number_of_lines):
+    #     if (total_states[i] != 0):
+    #         cascade_stop[i] = stable_states[i]/total_states[i]
+
+
+def load_initial_failures(initial_failure_table_name='initial_failures', number_of_lines=186):
+    # cell of arrays containing the initial failures for each iteration
+    initial_failures_mat = scipy.io.loadmat(initial_failure_table_name)
+    # print(initial_failures_mat['Initial_Failure_Table'])
+    initial_failures_arrays = [
+        l.tolist() for l in initial_failures_mat['Initial_Failure_Table']]
+    # print(initial_failures_arrays[:10])
+    # change initial failure matrix into list of lists
+    initial_failures = [l.tolist()[0] for l in initial_failures_arrays[0]]
+
+    return initial_failures
+    # print(initial_failures)
+    # print(mat)
+    # print(states_df)
+    # print("Iteration track = ", iteration_track) #check, should be the number of iterations run
+    # print(cluster_failures)
+    # print(cluster_failures_topological_factors)
+    # Cascading Failure Curve Code
+    # Plotting code
+    # cascading_failure_df = pd.DataFrame(
+    #     {'x_values': range(0, number_of_lines+1), 'cascade_stop': cascade_stop})
+    # return cascading_failure_df
+
+
+def generate_simulation_history_array(state_matrix, initial_failures) -> 'list[Simulation_Line_History]':
+
+    for i, single_sim_init_failures in enumerate(initial_failures):
+        print(i, single_sim_init_failures)
+
+
+state_matrix = load_state_matrix(SIM_STATE_MATRIX)
+initial_failures = load_initial_failures(SIM_INITIAL_FAILURES)
+
+# state_matrix = state_matrix[:100]
+initial_failures = initial_failures[:100]
+
+# print(state_matrix['Failed Line Index'])
+print(max(state_matrix['Failed Line Index']))
+print(initial_failures)
+
+# generate_simulation_history_array(state_matrix, initial_failures)
+print(state_matrix.shape)
+# print([state_matrix['Failed Line Index'][i]
+#   for i in range(state_matrix.shape[0])])
