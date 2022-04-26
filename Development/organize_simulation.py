bf54257cbddde7e7c5608b1b5dffb4d20274e269
nataliearns <natalie.arns@marquette.edu>
2022-04-26 14:23:40 -0500

    Completed organization function

--- Development/organize_simulation.py
+++ Development/organize_simulation.py
@@ -18,5 +18,9 @@ def organize_simulation(simulation_name, debug = False):
     os.rename('Simulation_Data' + os.sep +  simulation_name + os.sep + simulation_name + '_if', 'Simulation_Data' + os.sep +  simulation_name + os.sep + simulation_name + '_if')
     #move the dataframe file named [simulation_name]_df to the Simulation_Data/[simulation_name] folder
     os.rename('Simulation_Data' + os.sep +  simulation_name + os.sep + simulation_name + '_df', 'Simulation_Data' + os.sep +  simulation_name + os.sep + simulation_name + '_df')
-    
-    
+    #move the pstop file named [simulation_name]_pstop to the Simulation_Data/[simulation_name] folder
+    os.rename('Simulation_Data' + os.sep + simulation_name + os.sep + simulation_name + '_pstop', 'Simulation_Data' +os.sep + simulation_name + os.sep + simulation_name + '_pstop')
+    #move the simulation log file named [simulation_name]_log to the Simulation_Data/[simulation_name] folder
+    os.rename('Simulation_Data' + os.sep +  simulation_name + os.sep + simulation_name + '_log', 'Simulation_Data' + os.sep +  simulation_name + os.sep + simulation_name + '_log')
+    #move the end of simulation topology file named [simulation_name]_top to the Simulation_Data/[simulation_name]/[simulation_name]_tops folder
+    os.rename('Simulation_Data' + os.sep + simulation_name + os.sep + simulation_name + '_tops' + os.sep + simulation_name + '_top', 'Simulation_Data_tops' + os.sep + simulation_name + os.sep + simulation_name + '_tops' + os.sep + simulation_name + '_top')
